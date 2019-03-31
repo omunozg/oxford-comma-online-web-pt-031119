2ed0063a0124e8b86fdc8677cb27b6ffe161e952
Oscar Munoz <omunozg@emory.edu>
2019-03-31 21:08:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
-
+  if array.length == 1
+  array.join
+  
+  else if array.length == 2
+    array.join("and")
+    
+  else if array.length == 3
+    array.join(",")
+    array.insert(4, "and")
+    
+  else if array.length > 3
+    array.join(",")
+    array.insert(array.lenght -1, "and")
+    
+  else
+    
+    return array
+  end
+  
+    
+  return array
+  
 end
+
